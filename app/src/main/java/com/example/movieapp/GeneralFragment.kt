d8d42380fd261222925436b3ef03351f8e493823
ArmenHayrapetyan15 <armen.h.2005@gmail.com>
2022-05-25 16:33:51 +0400

    bottom navigation

--- app/src/main/java/com/example/movieapp/GeneralFragment.kt
+++ app/src/main/java/com/example/movieapp/GeneralFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.movieapp.Bottomnavigation.FavoriteFragment
+import com.example.movieapp.Bottomnavigation.HomeFragment
+import com.example.movieapp.Bottomnavigation.SettingsFragment
 import com.example.movieapp.databinding.FragmentGeneralBinding
 
 class GeneralFragment : Fragment() {
@@ -14,7 +17,7 @@ class GeneralFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentGeneralBinding.inflate(inflater)
         return binding.root
     }
@@ -22,6 +25,26 @@ class GeneralFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val homeFragment = HomeFragment()
+        val favoriteFragment = FavoriteFragment()
+        val settingsFragment = SettingsFragment()
 
+        makeCurrentFragment(homeFragment)
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.Home -> makeCurrentFragment(homeFragment)
+                R.id.Favorite -> makeCurrentFragment(favoriteFragment)
+                R.id.Settings -> makeCurrentFragment(settingsFragment)
+            }
+            true
+        }
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) {
+        fragmentManager?.beginTransaction()?.apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
     }
 }
